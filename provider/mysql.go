1dfc665dfc773ee0d50ecad5cc94bc66a0cfb48d
adamcanray <adamcool180@gmail.com>
2022-08-23 22:47:20 +0700

    MYSQL_PROVIDER_HOST and MYSQL_PROVIDER_PORT for mysql connection

--- provider/mysql.go
+++ provider/mysql.go
@@ -2,7 +2,9 @@ package provider
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -11,7 +13,15 @@ func MysqlProvider() *sql.DB {
 	// References:
 	// - ?parseTime=true -- https://stackoverflow.com/a/45040724/11587161
 	// - docker.for.mac.localhost -- https://stackoverflow.com/a/52504428/11587161
-	db, err := sql.Open("mysql", "root:root@tcp(docker.for.mac.localhost:3306)/go-mysql?parseTime=true")
+	// db, err := sql.Open("mysql", "root:root@tcp(docker.for.mac.localhost:3306)/go-mysql?parseTime=true")
+	db, err := sql.Open(
+		"mysql",
+		fmt.Sprintf(
+			"root:root@tcp(%s:%s)/go-mysql?parseTime=true",
+			os.Getenv("MYSQL_PROVIDER_HOST"),
+			os.Getenv("MYSQL_PROVIDER_PORT"),
+		),
+	)
 	if err != nil {
 		log.Fatalln("[provider.MysqlProvider]", err.Error())
 	}
